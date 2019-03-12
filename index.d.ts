a9c005c462c98076e9cc33c2521ff1ee61bf22f4
nav <wanglonghui@aerozhonghuan.com>
2019-03-12 13:50:29 +0800

    npm创建button库

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+
+declare module 'react-native-hh-button-view'{
+    import { ViewStyle } from 'react-native'
+    import {Component} from 'react'
+
+    export interface ButtonViewProps{
+        activeOpacity?: number,
+        title: string,
+        disabled?: boolean,
+        btnTitleStyle?: ViewStyle
+        btnViewStyle?: ViewStyle
+        onPressCallback?: () => void,
+    }
+    export default class ButtonView extends Component<SwiperProps, any> {
+    }
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+
+// import { NativeModules } from 'react-native';
+
+// const { RNHhbuttonview } = NativeModules;
+
+// export default RNHhbuttonview;
+import ButtonView from './src/'
+module.exports=ButtonView;
